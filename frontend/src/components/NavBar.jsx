5b8bf8fdf71d149f7bc84db9c6262c042f198ab3
ajamidar <jamidararnav@gmail.com>
2025-08-06 18:27:06 +0100

    setup complete

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import {assets} from '../assets/frontend_assets/assets'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <>
+        <div className='navbar-box'>
+            <div>
+                <img src={assets.logo} width={90} height={100}></img>
+                <p className='text-black'>hello</p>
+            </div>
+        </div>
+    </>
+
   )
 }
 
