35de905a982b33aeebe65063767a4a7a76c177a3
ajamidar <jamidararnav@gmail.com>
2025-08-08 23:00:37 +0100

    Building navBar

---
+++ frontend/src/assets/constants.js
@@ -0,0 +1,14 @@
+export const tempUniData = [
+    {
+        uniName : 'University of York',
+        location : 'Heslington, York, North Yorkshire, YO10 1DD'
+    },
+    {
+        uniName : 'University of Leicester',
+        location : 'Leicester, Leicestershire, LE2 XXX'
+    },
+    {
+        uniName : 'University of Southampton',
+        location : 'Southampton'
+    }
+]
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import {assets} from '../assets/frontend_assets/assets'
+import { NavLink } from 'react-router-dom'
+import { tempUniData } from '../assets/constants'
 
 const NavBar = () => {
+
+  console.log(tempUniData);
+  console.log('Hello');
   return (
     <>
         <div className='navbar-box'>
             <div className='flex flex-row justify-center items-center'>
-                <img src={assets.logo} width={90} height={100}></img>
+              <NavLink to='/'>
+              <img src={assets.logo} width={90} height={100}></img>
+              </NavLink>
             </div>
             <div className='location'>
-
+              <div className='location-text'>
+                <p>Select your University</p>
+              </div>
+              <div className='location-dropdown'>
+                <select name="location" id="location" aria-placeholder='Select Location'>
+                  {tempUniData.map((uni, index) => (
+                    <option key={index} value={uni?.uniName}>{uni?.uniName}</option>
+                  ))}
+                </select>
+              </div>
             </div>
             <div className='searchbar'>
 
