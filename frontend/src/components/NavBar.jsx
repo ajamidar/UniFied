d6c9cfb9f53d86e5f2ab38b5d874aef73a516804
ajamidar <jamidararnav@gmail.com>
2025-08-08 23:32:53 +0100

    location-dropdown

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -10,23 +10,27 @@ const NavBar = () => {
   return (
     <>
         <div className='navbar-box'>
-            <div className='flex flex-row justify-center items-center'>
+            <div className='scale-up flex flex-row justify-center items-center'>
               <NavLink to='/'>
-              <img src={assets.logo} width={90} height={100}></img>
+                <img src={assets.logo} width={90} height={100}></img>
               </NavLink>
             </div>
-            <div className='location'>
-              <div className='location-text'>
-                <p>Select your University</p>
+            <div className='scale-up border-2 border-[#33333311] flex flex-col items-center justify-center px-1 py-1 rounded-lg bg-gradient-to-b from-[#0073ff20] to-[#0194fdce]'>
+              <div className='font-bold text-[1.2rem]'>
+                <p>Your University</p>
               </div>
               <div className='location-dropdown'>
-                <select name="location" id="location" aria-placeholder='Select Location'>
+                <select name="location" id="location" defaultValue='' className='cursor-pointer'>
+                  <option value="" disabled>
+                    Choose one...
+                  </option>
                   {tempUniData.map((uni, index) => (
                     <option key={index} value={uni?.uniName}>{uni?.uniName}</option>
                   ))}
                 </select>
               </div>
             </div>
+
             <div className='searchbar'>
 
             </div>
