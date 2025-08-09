178147ec469e582444f62424e0f5affe95f0cfa3
ajamidar <jamidararnav@gmail.com>
2025-08-09 23:19:30 +0100

    Update NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,8 +5,6 @@ import { tempUniData } from '../assets/constants'
 
 const NavBar = () => {
 
-  console.log(tempUniData);
-  console.log('Hello');
   return (
     <>
         <div className='navbar-box'>
@@ -15,12 +13,12 @@ const NavBar = () => {
                 <img src={assets.logo} width={90} height={100}></img>
               </NavLink>
             </div>
-            <div className='scale-up border-2 border-[#42424211] flex flex-col items-center justify-center px-1 py-2 rounded-lg bg-gradient-to-b from-[#0073ff0f] to-[#0194fd4b]'>
+            <div className='scale-up border-2 border-[#42424211] flex flex-col gap-1 items-center justify-center px-1 py-2 rounded-lg bg-gradient-to-b from-[#0073ff0f] to-[#0194fd4b]'>
               <div className='font-bold text-[1.1rem] flex flex-row gap-1 items-center'>
                 <p>Your University</p>
                 <img src='/location.svg' width={15} height={15} className='pb-0.5'></img>
               </div>
-              <div className='location-dropdown border-1 border-[#333333] rounded-2xl text-[0.9rem] text-[#333333] px-2'>
+              <div className='location-dropdown border-1 border-[#333333] rounded-2xl mb-1 text-[0.9rem] text-[#333333] px-2'>
                 <select name="location" id="location" defaultValue='' className='cursor-pointer'>
                   <option value="" disabled>
                     Choose one...
