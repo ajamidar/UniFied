a5a657025f22866c356674d20683dceaf71b5429
ajamidar <jamidararnav@gmail.com>
2025-08-09 23:09:49 +0100

    Update constants.js

--- frontend/src/assets/constants.js
+++ frontend/src/assets/constants.js
@@ -9,6 +9,14 @@ export const tempUniData = [
     },
     {
         uniName : 'University of Southampton',
-        location : 'Southampton'
+        location : 'Southampton, Hampshire, SO17 1BJ'
+    },
+    {
+        uniName : 'University of Birmingham',
+        location : 'Birmingham, West Midlands, B15 2TT'
+    },
+    {
+        uniName : 'University of Manchester',
+        location : 'Manchester, Greater Manchester, M13 9PL'
     }
 ]
