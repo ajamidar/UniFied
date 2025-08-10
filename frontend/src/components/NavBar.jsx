751b40ad13282374f076b09ad4976153b3baf815
ajamidar <jamidararnav@gmail.com>
2025-08-11 00:01:59 +0100

    Update NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -28,20 +28,22 @@ const NavBar = () => {
               </div>
               <div className='location-dropdown border-1 border-[#333333] rounded-2xl mb-1 text-[0.85rem] text-[#333333] px-2'>
                 <select name="location" id="location" value={selectedUni} onChange={e => setSelectedUni(e.target.value)} className='cursor-pointer'>
-                  <option value="" disabled>
-                    Choose one...
-                  </option>
-                  {tempUniData.map((uni, index) => (
-                    <option key={index} value={uni?.uniName}>{uni?.uniName}</option>
-                  ))}
+                    <option value="" disabled>
+                      Choose one...
+                    </option>
+                    {tempUniData.map((uni, index) => (
+                      <option key={index} value={uni?.uniName}>{uni?.uniName}</option>
+                    ))} 
                 </select>
               </div>
             </div>
             <div className='searchbar w-3/8 flex flex-row items-center justify-between  rounded-lg '>
               <input type='text' placeholder='Search...' value={searchValue} onChange={e => setSearchValue(e.target.value)} className='w-full h-full pl-3 py-3 rounded-tl-lg rounded-bl-lg bg-[#f0f0f0] border border-[#ccc]' />
-              <button type='button' onClick={handleSearch} className=' px-4 py-2.5 rounded-tr-lg rounded-br-lg bg-[#ffaa01] text-white hover:bg-[#ed9a26] transition '>
-                <img src='/search.svg' width={27} height={27} alt='Search' />
-              </button>
+              <NavLink to="/collection">
+                <button type='button' onClick={handleSearch} className=' px-4 py-2.5 rounded-tr-lg rounded-br-lg bg-[#ffaa01] text-white hover:bg-[#ed9a26] transition '>
+                  <img src='/search.svg' width={30} height={30} alt='Search' />
+                </button>
+              </NavLink>
             </div>
             <div className='sign-in/account'>
               <NavLink to='/login'>
@@ -51,7 +53,7 @@ const NavBar = () => {
               </NavLink>
             </div>
             <div className='cart'>
-              <NavLink to='/basket' className='flex flex-row items-center justify-center gap-2'>                  
+              <NavLink to='/cart' className='flex flex-row items-center justify-center gap-2'>                  
                   <img src='/cart.svg' width={45} height={45} alt='Basket' />
                   <p className='text-[1.25rem] font-bold pt-3'>Cart ({num})</p>  
               </NavLink>
