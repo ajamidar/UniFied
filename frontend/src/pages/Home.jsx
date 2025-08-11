0c3b91800452f1ee52c46007d9b3ca5459ff2198
ajamidar <jamidararnav@gmail.com>
2025-08-11 23:12:44 +0100

    Policies displayed on landing page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import LandingPictures from '../components/LandingPictures'
 import SmallLandingPictures from '../components/SmallLandingPictures'
 import LatestCollection from '../components/LatestCollection'
+import OurPolicy from '../components/OurPolicy'
 
 const Home = () => {
   return (
     <>
     <div><LandingPictures /></div>
     <div><SmallLandingPictures /></div>
-    <div><LatestCollection /></div>
+    {/*<div><LatestCollection /></div>*/}
+    <div><OurPolicy /></div>
     </>
     
   )
