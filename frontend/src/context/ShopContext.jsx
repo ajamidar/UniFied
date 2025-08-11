d2fabe2cd214135f5ae60a4973ee1e54a215a388
ajamidar <jamidararnav@gmail.com>
2025-08-11 22:35:16 +0100

    Product render sorted

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,22 @@
+import { createContext } from "react";
+import { products } from "../assets/frontend_assets/assets";
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = ({ children }) => {
+
+    const currency = '£';
+
+    const value = {
+        products , currency
+    }
+
+    return (
+        <ShopContext.Provider value={value}>
+            {children}
+        </ShopContext.Provider>
+    )
+
+}
+
+export default ShopContextProvider;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import Hero from '../components/Hero'
 import LandingPictures from '../components/LandingPictures'
 import SmallLandingPictures from '../components/SmallLandingPictures'
+import LatestCollection from '../components/LatestCollection'
 
 const Home = () => {
   return (
     <>
     <div><LandingPictures /></div>
     <div><SmallLandingPictures /></div>
+    <div><LatestCollection /></div>
     </>
     
   )
