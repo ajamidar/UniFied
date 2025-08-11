abfc99b57b995ac9982c9bcf68f370c1c2d4caf7
ajamidar <jamidararnav@gmail.com>
2025-08-11 23:23:45 +0100

    mobile-styling for policies

--- frontend/src/components/OurPolicy.jsx
+++ frontend/src/components/OurPolicy.jsx
@@ -3,21 +3,21 @@ import { assets } from '../assets/frontend_assets/assets'
 
 const OurPolicy = () => {
   return (
-    <div className='flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-5 text-xs sm:text-sm md:text-base text-[#333333]'>
+    <div className='flex flex-row sm:flex-row justify-around gap-12 max-sm:gap-2 text-center py-5 max-sm:py-2 text-xs sm:text-sm md:text-base text-[#333333]'>
         <div>
-            <img src={assets.exchange_icon} className='w-12 m-auto mb-5' alt="Exchange Icon" />
-            <p className='font-semibold'>Exchange Policy</p>
-            <p className='text-gray-400'>We offer hassle free exhange policy</p>
+            <img src={assets.exchange_icon} className='w-12 max-sm:w-7 m-auto mb-5 max-sm:mb-2 max-sm:size-[30px]' alt="Exchange Icon" />
+            <p className='font-semibold max-sm:text-[0.7rem]'>Exchange Policy</p>
+            <p className='text-gray-400 max-sm:text-[0.6rem]'>We offer hassle free exhange policy</p>
         </div>
         <div>
-            <img src={assets.quality_icon} className='w-12 m-auto mb-5' alt="Exchange Icon" />
-            <p className='font-semibold'>Quicks Returns Policy</p>
-            <p className='text-gray-400'>We offer free 7-day returns</p>
+            <img src={assets.quality_icon} className='w-12 max-sm:w-7 m-auto mb-5 max-sm:mb-2 max-sm:size-[30px]' alt="Exchange Icon" />
+            <p className='font-semibold max-sm:text-[0.7rem]'>Quicks Returns Policy</p>
+            <p className='text-gray-400 max-sm:text-[0.6rem]'>We offer free 7-day returns</p>
         </div>
         <div>
-            <img src={assets.support_img} className='w-12 m-auto mb-5' alt="Exchange Icon" />
-            <p className='font-semibold'>Best Customer Support</p>
-            <p className='text-gray-400'>We provide 24/7 customer service</p>
+            <img src={assets.support_img} className='w-12 max-sm:w-7 m-auto mb-5 max-sm:mb-2 max-sm:size-[30px]' alt="Exchange Icon" />
+            <p className='font-semibold max-sm:text-[0.7rem]'>Best Customer Support</p>
+            <p className='text-gray-400 max-sm:text-[0.6rem]'>We provide 24/7 customer service</p>
         </div>
     </div>
   )
--- frontend/src/pages/UniHome.jsx
+++ frontend/src/pages/UniHome.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import LatestCollection from '../components/LatestCollection';
 
 const UniHome = () => {
     const {selectedUni} = useParams();
 
   return (
     <>
-    <h1 className='font-extrabold'>{selectedUni}'s Bestsellers</h1>
+    <h1 className='font-extrabold'>{selectedUni}'s Home Page</h1>
+    <LatestCollection />
     </>
   )
 }
