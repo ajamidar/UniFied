e4d9c458e0fa8a19eaf53530e1e3bd6d787af7cf
ajamidar <jamidararnav@gmail.com>
2025-08-12 19:31:23 +0100

    more styling

--- frontend/src/components/NewsLetterBox.jsx
+++ frontend/src/components/NewsLetterBox.jsx
@@ -13,9 +13,9 @@ const NewsLetterBox = () => {
     <div className='text-center'>
         <p className='text-2xl font-medium text-gray-800 max-sm:text-[1.2rem]'>Subscribe now and get 20% off</p>
         <p className='text-gray-400 mt-2 max-sm:text-[0.7rem]'>Get latest updates about new listings, price-drops, coupon code straight to your inbox.</p>
-        <form onSubmit={onSubmitHandler} className='w-full max-sm:w-3/4 flex items-center gap-3 mx-auto my-6 border pl-3'>
-            <input type='email' placeholder='Enter your email' className='w-full sm:flex-1 outline-none max-sm:text-[0.6rem]'  required/>
-            <button type='submit' className='bg-[#00b3ff] font-medium text-white px-4 py-2 mt-3 max-sm:mt-0 sm:ml-2 max-sm:text-[0.65rem]'>Subscribe</button>
+        <form onSubmit={onSubmitHandler} className='w-3/4 min-sm:w-3/5 flex items-center gap-3 mx-auto my-6 border pl-3'>
+            <input type='email' placeholder='Enter your email' className='w-3/4 sm:flex-1 outline-none max-sm:text-[0.6rem]'  required/>
+            <button type='submit' className='bg-[#00b3ff] font-medium text-white px-4 py-2 sm:ml-2 max-sm:text-[0.65rem]'>Subscribe</button>
         </form>
     </div>
   )
