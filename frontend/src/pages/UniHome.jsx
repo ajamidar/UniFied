61ad122329efd39ca7bd9a2278fe3147cfc9b982
ajamidar <jamidararnav@gmail.com>
2025-08-14 23:37:03 +0100

    Update UniHome.jsx

--- frontend/src/pages/UniHome.jsx
+++ frontend/src/pages/UniHome.jsx
@@ -3,11 +3,14 @@ import { useParams } from 'react-router-dom'
 import LatestCollection from '../components/LatestCollection';
 import GradientText from '../components/GradientText';
 import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/frontend_assets/assets';
+import Title from '../components/Title';
 
 const UniHome = () => {
     const {selectedUni} = useParams();
     const { products } = useContext(ShopContext);
     const [showFilter, setShowFilter] = useState(false);
+    const [filteredProducts, setFilteredProducts] = useState([]);
 
   return (
     <>
@@ -28,8 +31,10 @@ const UniHome = () => {
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10'>
 
       {/*Filter Section*/}
-      <div className='min-w-60'>
-        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTER</p>
+      <div className='min-w-50'>
+        <p onClick={() => setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2 pl-2'>FILTERS
+          <img src={assets.dropdown_icon} alt='dropdown icon' className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`}/>
+        </p>
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
           <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
@@ -62,6 +67,26 @@ const UniHome = () => {
             </p>
           </div>
         </div>
+      </div>
+      {/*Right Section*/}
+      <div className='flex-1'>
+
+        <div className='flex justify-between text-base sm:text-2xl mb-4'>
+          <Title text1={'ALL'} text2={'PRODUCTS'} />
+          {/*Product Sorting*/}
+          <select className='border-2 border-gray-300 text-sm px-2'>
+            <option value="relavence">Sort by: Relavence</option>
+            <option value="">Sort by: Price - Low to High</option>
+            <option value="">Sort by: Price - High to Low</option>
+          </select>
+
+        {/*Map Products*/}
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
+
+        </div>
+
+        </div>
+
       </div>
 
     </div>
