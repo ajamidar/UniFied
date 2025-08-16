0f875f04f0384fdb07cef40fbfb498b01cb5d89c
ajamidar <jamidararnav@gmail.com>
2025-08-16 23:12:22 +0100

    Update UniHome.jsx

--- frontend/src/pages/UniHome.jsx
+++ frontend/src/pages/UniHome.jsx
@@ -47,10 +47,6 @@ const UniHome = () => {
     }
 
     useEffect(() => {
-      setFilteredProducts(products);
-    },[])
-
-    useEffect(() => {
       applyFilters();
     }, [category, subCategory]);
 
