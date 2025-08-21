40051ff28ec511fea2305c6484997890b9e8208f
ajamidar <jamidararnav@gmail.com>
2025-08-21 16:07:52 +0100

    Update Product.jsx

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
+import { p } from 'motion/react-client';
 
 const Product = () => {
 
@@ -10,6 +11,13 @@ const Product = () => {
 
   const fetchProductData = async () => {
 
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item)
+        console.log(item);
+        return null;
+      }
+    })
   }
 
   useEffect(() => {
