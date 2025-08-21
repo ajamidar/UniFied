bb003d9ae5cef2467df16bebe016bd0b7fc3db26
ajamidar <jamidararnav@gmail.com>
2025-08-21 13:45:42 +0100

    Update Product.jsx

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Product = () => {
 
   const { productId } = useParams();
+  const { products } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+
+  const fetchProductData = async () => {
+
+  }
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId,products])
 
   return (
     <div>Product</div>
