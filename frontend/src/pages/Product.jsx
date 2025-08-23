39b5ed8c049b1a34aaf5e259a2492ea7751d9209
ajamidar <jamidararnav@gmail.com>
2025-08-23 22:55:19 +0100

    Update Product.jsx

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -31,7 +31,11 @@ const Product = () => {
         {/* Product Image */}
         <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
           <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
-            
+            {
+              productData.image.map((item, index) => (
+                <img key={index} src={item} alt={`Product Image ${index + 1}`} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' />
+              ))
+            }
           </div>
         </div>
       </div>
