05b168e2fca839dc9b4f001752645fb69f078faf
ajamidar <jamidararnav@gmail.com>
2025-08-24 17:40:58 +0100

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] ] gap-14 my-10 text-sm max-sm:gap-5'>
 
             <div>
-                <img src='main-logo.png' alt='logo' className='mb-3 w-25 max-sm:w-15' />
+                <img src={assets.logo} alt='logo' className='mb-3 w-25 max-sm:w-15' />
                 <p className='w-full md:w-2/3 text-gray-600 max-sm:text-xs'>
                     We are dedicated to creating the most trusted and convenient platform for buying and selling preloved items within your university community. Our mission is to make it easy for students to find great deals and connect with others in a safe, familiar environment. Whether you’re looking to declutter, save money, we’re here to make the process smooth — all while fostering a stronger sense of community on campus
                 </p>
