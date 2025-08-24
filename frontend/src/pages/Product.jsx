62f92a8dbd07c67807a94176f83594d8346ae4a3
ajamidar <jamidararnav@gmail.com>
2025-08-24 17:21:16 +0100

    Update Product.jsx

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/frontend_assets/assets';
 
 const Product = () => {
 
@@ -14,6 +15,7 @@ const Product = () => {
     products.map((item) => {
       if (item._id === productId) {
         setProductData(item)
+        setImage(item.image[0])
         console.log(item);
         return null;
       }
@@ -26,18 +28,41 @@ const Product = () => {
 
   return productData ? (
     <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+
       {/* Product Details */}
+
       <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row justify-center items-center'>
+
         {/* Product Image */}
+
         <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
           <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
             {
               productData.image.map((item, index) => (
-                <img key={index} src={item} alt={`Product Image ${index + 1}`} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' />
+                <img onClick={() => setImage(item)} key={index} src={item} alt={`Product Image ${index + 1}`} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' />
               ))
             }
           </div>
+          <div className='w-full sm:w-[80%]'>
+            <img src={image} alt={`Product Image Main`} className='w-full h-auto' />
+          </div>
         </div>
+
+        {/*Product Info*/}
+        <div className='flex-1'>
+          <h1 className='text-2xl mt-2 font-medium'>{productData.name}</h1>
+          <div className='flex items-center gap-1 mt-2'>
+            <h2>Seller rating:</h2>
+            <img src={assets.star_icon} alt="star-icon" className='w-3 5'/>
+            <img src={assets.star_icon} alt="star-icon" className='w-3 5'/>
+            <img src={assets.star_icon} alt="star-icon" className='w-3 5'/>
+            <img src={assets.star_icon} alt="star-icon" className='w-3 5'/>
+            <img src={assets.star_dull_icon} alt="star-icon" className='w-3 5'/>
+            <p className='pl-1'>(122) reviews</p>
+          </div>
+          <p></p>
+        </div>
+
       </div>
 
     </div>
