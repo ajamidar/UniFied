c05626314b1b68527d5fb98364a98e615ad751ff
ajamidar <jamidararnav@gmail.com>
2025-08-28 18:13:31 +0100

    Update ShopContextContext.jsx

--- frontend/src/context/ShopContextContext.jsx
+++ frontend/src/context/ShopContextContext.jsx
@@ -2,4 +2,4 @@ import { createContext } from "react";
 
 export const ShopContext = createContext();
 
-//Separate file for context provider to make sure export is compatible and Fast refresh can happen.
+//Separate file for context provider to make sure export is compatible; Avoid Fast Refresh errors.
