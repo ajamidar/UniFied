d5dc36a9ececfed51ad33a0a4c06e29a928a23ce
ajamidar <jamidararnav@gmail.com>
2025-08-29 21:24:51 +0100

    Update Cart.jsx

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContextContext';
+import Title from '../components/Title';
 
 const Cart = () => {
 
@@ -21,12 +22,39 @@ const Cart = () => {
         }
       }
     }
-    console.log(tempData);
+    setCartData(tempData);
 
   }, [cartItems]);
 
   return (
-    <div>Cart</div>
+    <div className='border-t pt-14'>
+
+      <div className='text-2xl mb-3'>
+        <Title text1={'YOUR'} text2={'CART'} />
+      </div>
+
+      <div>
+        {
+          cartData.map((item, index) => {
+
+            const productData = products.find((product)=> product._id === item._id);
+
+            return (
+              <div key={index} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
+                <div className='flex items-start gap-6'>
+                  <img src={productData.image[0]} alt="product image" className='w-16 sm:w-20'/>
+                  <div>
+                    <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
+                  </div>
+                </div>
+              </div>
+            )
+
+          })
+        }
+      </div>
+      
+    </div>
   )
 
 }
