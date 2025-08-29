072f2315ddc4bd5296858b69b337c797c977c2c8
ajamidar <jamidararnav@gmail.com>
2025-08-29 20:58:47 +0100

    Update Cart.jsx

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContextContext';
 
 const Cart = () => {
+
+  const { products, currency, cartItems } = useContext(ShopContext);
+
   return (
     <div>Cart</div>
   )
+
 }
 
 export default Cart
