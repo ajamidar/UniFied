3fdafd5359a77dbae29f1f465ec73b17fbb44199
ajamidar <jamidararnav@gmail.com>
2025-09-05 21:25:51 +0100

    footer changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Collection from "./pages/Collection"
-import About from "./pages/About"
 import Contact from "./pages/Contact"
 import Product from "./pages/Product"
 import Cart from "./pages/Cart"
@@ -24,7 +23,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/:selectedUni" element={<UniHome />} />
           <Route path="/collection" element={<Collection />} />
-          <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/:selectedUni/product/:productId" element={<Product />} />
           <Route path="/:selectedUni/cart" element={<Cart />} />
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,47 +1,64 @@
 import React from 'react'
 import { assets } from '../assets/frontend_assets/assets'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const Footer = () => {
-  return (
-    <div className='max-sm:mb-5'>
-        <hr className='mt-30 max-sm:mt-10' />
-        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] ] gap-14 my-10 text-sm max-sm:gap-5'>
+
+    const navigate = useNavigate();
+
+    return (
+        <div className='max-sm:mb-5'>
+            <hr className='mt-30 max-sm:mt-10' />
+            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] ] gap-14 my-10 text-sm max-sm:gap-5'>
+
+                <div>
+                    <img src={assets.logo} alt='logo' className='mb-3 w-25 max-sm:w-15' />
+                    <p className='w-full md:w-2/3 text-gray-600 max-sm:text-xs'>
+                        We are dedicated to creating the most trusted and convenient platform for buying and selling preloved items within your university community. Our mission is to make it easy for students to find great deals and connect with others in a safe, familiar environment. Whether you’re looking to declutter, save money, we’re here to make the process smooth — all while fostering a stronger sense of community on campus
+                    </p>
+                </div>
+
+                <div>
+                    <p className='text-xl font-medium mb-5 max-sm:text-sm'>COMPANY</p>
+                    <ul className='text-gray-600 flex flex-col gap-1 max-sm:text-xs'>
+
+                            <NavLink to='/'>
+                                <li>Home</li>
+                            </NavLink>
+
+                            <NavLink to='/login'>
+                                <li>Login</li>
+                            </NavLink>
+
+                            <NavLink to='/login'>
+                                <li>Collection</li>
+                            </NavLink>
+
+                            <NavLink to='/'>
+                                <li>Privacy policy</li>
+                            </NavLink>
+                        
+                    </ul>
+                </div>
+
+                <div>
+                    <p className='text-xl font-medium mb-5 max-sm:text-sm'>GET IN TOUCH</p>
+                    <ul className='text-gray-600 flex flex-col gap-1 max-sm:text-xs'>
+                        <li>+44 7443735548</li>
+                        <li>contact@unified.com</li>
+                    </ul>
+                </div>
+
+            </div>
 
             <div>
-                <img src={assets.logo} alt='logo' className='mb-3 w-25 max-sm:w-15' />
-                <p className='w-full md:w-2/3 text-gray-600 max-sm:text-xs'>
-                    We are dedicated to creating the most trusted and convenient platform for buying and selling preloved items within your university community. Our mission is to make it easy for students to find great deals and connect with others in a safe, familiar environment. Whether you’re looking to declutter, save money, we’re here to make the process smooth — all while fostering a stronger sense of community on campus
+                <hr />
+                <p className='py-5 text-sm text-center max-sm:text-xs'>
+                    Copyright 2025 @ UniFied - All rights reserved.
                 </p>
             </div>
-
-            <div>
-                <p className='text-xl font-medium mb-5 max-sm:text-sm'>COMPANY</p>
-                <ul className='text-gray-600 flex flex-col gap-1 max-sm:text-xs'>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Collection</li>
-                    <li>Privacy policy</li>
-                </ul>
-            </div>
-
-            <div>
-                <p className='text-xl font-medium mb-5 max-sm:text-sm'>GET IN TOUCH</p>
-                <ul className='text-gray-600 flex flex-col gap-1 max-sm:text-xs'>
-                    <li>+44 7443735548</li>
-                    <li>contact@unified.com</li>
-                </ul>
-            </div>
-
         </div>
-
-        <div>
-            <hr />
-            <p className='py-5 text-sm text-center max-sm:text-xs'>
-                Copyright 2025 @ UniFied - All rights reserved.
-            </p>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Footer
